f3e157052e4fabc9dcd231c5170387cc1a57fc2b
xavier <xavier.deredec@live.fr>
2021-09-27 19:44:42 +0200

    Add folder view and responsive to app

---
+++ src/views/Folder.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="main">
+    <HeadBar />
+    <router-link to="/" class="folder">
+      <div class="head">Projets</div>
+      <div class="project--list">
+        <div class="row">
+          <div class="project">
+            <div class="app"></div>
+            <div class="title">DjKiwixx</div>
+          </div>
+          <div class="project">
+            <div class="app"></div>
+            <div class="title">Ae Pierrefeu</div>
+          </div>
+        </div>
+      </div>
+    </router-link>
+  </div>
+</template>
+<script>
+import HeadBar from "@/components/HeadBar.vue";
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {
+    HeadBar,
+  },
+})
+export default class Folder extends Vue {}
+</script>
+<style lang="scss" scoped>
+.main {
+  background: url("../assets/wallpaper.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-x: 92%;
+  min-height: 100vh;  
+  .folder {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 40px);
+    text-decoration: none;
+    .head {
+      font-family: "Helvetica";
+      font-weight: 400;
+      color: #fff;
+      font-size: 28px;
+    }
+    .project--list {
+      width: 90%;
+      height: 300px;
+      background: rgba(144, 144, 144, 0.25);
+      backdrop-filter: blur(4px);
+      border-radius: 30px;
+      .row {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 5%;
+        padding-top: 5%;
+        padding-left: 5%;
+        place-items: center;
+        .project {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          &:nth-child(1)>.app{
+              background: url('../assets/djkiwixx.png');
+          }
+          &:nth-child(2)>.app{
+              background: url('../assets/ae.png') #fff;
+          }
+          .app{
+              width: 60px;
+              height: 60px;
+              border-radius: 10px;
+              background-size: cover !important;
+          }
+          .title{
+              font-family: 'Helvetica';
+              font-weight: 400;
+              color: #fff;
+              font-size: 14px;
+              text-align: center;
+          }
+        }
+      }
+    }
+  }
+}
+@media screen and (min-width:360px) {
+    .project--list{
+        height: 350px !important;
+    }
+}
+@media screen and (min-width:768px) {
+    .head{
+        font-size: 42px !important;
+    }
+    .project--list {
+        width: 70% !important;
+        height: 500px !important;
+        .row{
+            grid-template-columns: 1fr 1fr 1fr 1fr !important;
+            .app{
+                width: 80px !important;
+                height: 80px !important;
+            }
+            .title{
+                font-size: 1.2em !important;
+            }
+        }
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,13 @@
   <div class="main">
     <HeadBar />
     <div class="row">
-      <div class="project">
+      <router-link to="/projet" class="project">
         <div class="folder">
-          <img src="../assets/dj-min.png" />
-          <img src="../assets/ae-min.png" />
+          <div class="dj"></div>
+          <div class="ae"></div>
         </div>
         <div class="head">Projets</div>
-      </div>
+      </router-link>
       <div class="skills">
         <a class="app">
           <Icon icon="simple-icons:appstore" color="white" height="90%" />
@@ -52,18 +52,19 @@ export default class Home extends Vue {}
   .row {
     width: 100%;
     padding-top: 10px;
-    padding-left: 10px;
+    padding-left: 5px;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     .project {
+      text-decoration: none;
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
       .folder {
-        padding: 10px;
-        height: 60px;
-        width: 60px;
+        padding: 5px;
+        height: 50px;
+        width: 50px;
         background: rgba(144, 144, 144, 0.25);
         backdrop-filter: blur(4px);
         border-radius: 10px;
@@ -72,9 +73,18 @@ export default class Home extends Vue {}
         grid-template-rows: 1fr 1fr 1fr;
         gap: 10%;
         place-items: center;
-        img {
-          width: 100%;
-          height: 100%;
+        .dj,
+        .ae {
+          width: 15px;
+          height: 15px;
+          border-radius: 2.5px;
+          background-size: cover !important;
+        }
+        .dj {
+          background: url("../assets/djkiwixx.png");
+        }
+        .ae {
+          background: url("../assets/ae.png") #fff;
         }
       }
       .head {
@@ -91,9 +101,9 @@ export default class Home extends Vue {}
       a {
         background: linear-gradient(180deg, #6dcbff 0%, #14426c 100%);
         border-radius: 10px;
-        height: 60px;
-        width: 60px;
-        padding: 10px;
+        height: 50px;
+        width: 50px;
+        padding: 5px;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -110,9 +120,9 @@ export default class Home extends Vue {}
       align-items: center;
       justify-content: center;
       .app {
-        height: 60px;
-        width: 60px;
-        padding: 10px;
+        height: 50px;
+        width: 50px;
+        padding: 5px;
         border-radius: 10px;
         background: url("../assets/setting.jpg");
         background-size: cover;
@@ -124,9 +134,22 @@ export default class Home extends Vue {}
         font-family: "Helvetica";
       }
     }
-    @media screen and (min-width:1280px) {
-      display: none;
-    }
+  }
+}
+@media screen and (min-width: 768px) {
+  .row {
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr !important;
+  }
+  .project > .folder,
+  .skills > .app,
+  .about > .app {
+    width: 60px !important;
+    height: 60px !important;
+  }
+}
+@media screen and (min-width: 1280px) {
+  .row {
+    display: none !important;
   }
 }
 </style>
